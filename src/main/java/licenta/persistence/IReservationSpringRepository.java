e2c2a0660c29d4313f0e4f760a42d4f521af18d1
Patrick790 <136745353+Patrick790@users.noreply.github.com>
2025-06-15 16:20:27 +0300

    optimized prioritization 2

--- src/main/java/licenta/persistence/IReservationSpringRepository.java
+++ src/main/java/licenta/persistence/IReservationSpringRepository.java
@@ -1,24 +1,19 @@
 package licenta.persistence;
 
+import jakarta.transaction.Transactional;
 import licenta.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-import java.util.List;
 
-public interface IReservationSpringRepository extends JpaRepository<Reservation, Long> {
+public interface IReservationSpringRepository extends CrudRepository<Reservation, Long> {
 
     @Modifying
     @Transactional
     @Query("DELETE FROM Reservation r WHERE r.date >= :startDate AND r.date <= :endDate AND r.type = 'reservation'")
     int deleteReservationsByDateRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
-    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.date BETWEEN :startDate AND :endDate AND r.type = 'reservation'")
-    long countByDateBetweenAndType(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("type") String type);
-
-    List<Reservation> findByDateBetweenAndType(Date startDate, Date endDate, String type);
 }
